456f59403d90450efc6813af1b91e52eca48fabc
PaKyxs <rodrigomoreiraginarte@gmail.com>
2024-02-07 17:13:09 -0300

    'debug host'

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import pandas as pd
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '¡La aplicación está en funcionamiento!'
+
 @app.route('/wine-data')
 def get_wine_data():
     url = 'https://storage.googleapis.com/the_public_bucket/wine-clustering.csv'
@@ -10,4 +14,4 @@ def get_wine_data():
     return df.to_json()
 
 if __name__ == '__main__':
-    app.run(debug=False, host='127.0.0.1')
+    app.run(debug=True, host='0.0.0.0')
--- clustering.py
+++ clustering.py
@@ -8,22 +8,33 @@ from sklearn.cluster import KMeans
 import requests
 
 def clustering():
-    # Retrieve wine data from API
-    response = requests.get('http://172.17.0.2:5000/wine-data')
-    df = pd.read_json(response.text)
+    # Hacer la solicitud HTTP a la ruta /wine-data
+    response = requests.get('http://127.0.0.1:5000/wine-data')
     
-    # Standardize the features
-    scaler = StandardScaler()
-    data_scaled = scaler.fit_transform(df)
+    # Verificar si la solicitud fue exitosa
+    if response.status_code == 200:
+        # Leer los datos JSON de la respuesta
+        wine_data_json = response.json()
+        
+        # Convertir los datos JSON a un DataFrame
+        df = pd.read_json(wine_data_json)
+        
+        # Standardize the features
+        scaler = StandardScaler()
+        data_scaled = scaler.fit_transform(df)
 
-    # Apply PCA (Principal Component Analysis) for dimensionality reduction
-    pca = PCA(n_components=2)  
-    data_pca = pca.fit_transform(data_scaled)
+        # Apply PCA (Principal Component Analysis) for dimensionality reduction
+        pca = PCA(n_components=2)  
+        data_pca = pca.fit_transform(data_scaled)
 
-    # Apply K-means clustering
-    kmeans = KMeans(n_clusters=3, random_state=42)
-    df['Wine_Type'] = kmeans.fit_predict(data_pca)
-    print(df)
+        # Apply K-means clustering
+        kmeans = KMeans(n_clusters=3, random_state=42)
+        df['Wine_Type'] = kmeans.fit_predict(data_pca)
+        
+        # Imprimir el DataFrame después de realizar el clustering
+        print(df)
+    else:
+        print("Error: No se pudo obtener los datos de vino.")
 
 if __name__ == '__main__':
     clustering()
