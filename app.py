9d4689f83859043fb7715a10d08f477fa8c41aa6
PaKyxs <rodrigomoreiraginarte@gmail.com>
2024-02-08 00:24:48 -0300

    'index template added, clustering.py removed'

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '¡La aplicación está en funcionamiento!'
+    return render_template('index.html')
 
 @app.route('/wine-data')
 def get_wine_data():
@@ -13,5 +19,30 @@ def get_wine_data():
     df = pd.read_csv(url)
     return df.to_json()
 
+@app.route('/clustering')
+def clustering():
+
+    wine_data_json = get_wine_data()
+    
+    # Convertir los datos JSON a un DataFrame
+    df = pd.read_json(wine_data_json)
+
+    # Standardize the features
+    scaler = StandardScaler()
+    data_scaled = scaler.fit_transform(df)
+
+    # Apply PCA (Principal Component Analysis) for dimensionality reduction
+    pca = PCA(n_components=2)  
+    data_pca = pca.fit_transform(data_scaled)
+
+    # Apply K-means clustering
+    kmeans = KMeans(n_clusters=3, random_state=42)
+    df['Wine_Type'] = kmeans.fit_predict(data_pca)
+    
+    # Imprimir el DataFrame después de realizar el clustering
+    return df.to_json()
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+    clustering()
